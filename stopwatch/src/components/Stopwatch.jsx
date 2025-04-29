73c8b746850d25a4d5053e0ed2d942ef2c4b4713
Bhupesh Kumar <kumarbhupesh800@gmail.com>
2025-04-29 20:46:37 +0530

    Add state for time and stopwatch status

--- stopwatch/src/components/Stopwatch.jsx
+++ stopwatch/src/components/Stopwatch.jsx
@@ -1,14 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import "./stopwatch.css";
 
 function Stopwatch() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+
   return (
     <div className="stopwatch-container">
       <h1 className="stopwatch-title">Stopwatch</h1>
       <div className="stopwatch-time">00:00:00.00</div>
       <div className="stopwatch-controls">
-        <button className="start-stop-btn">Start</button>
+        <button className="start-stop-btn">{isRunning ? "Stop" : "Start"}</button>
         <button className="reset-btn">Reset</button>
       </div>
     </div>
